d93f1649112c28d4b07429e7a59ba7a76a249f74
swillrich5 <swillrich@gmail.com>
2021-10-30 18:57:11 -0500

    primitive content index and detail views set up and working

--- content/models.py
+++ content/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Content(models.Model):
+    subject = models.CharField(max_length=100)
+    content = models.TextField()
+    created_on = models.DateField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.subject[:50]
+
--- content/views.py
+++ content/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from .models import Content
+
+def index(request):
+    content_list = Content.objects.all()
+    context = {
+        'content_list': content_list,
+    }
+    return render(request, 'content/index.html', context)
+
+
+def detail(request, content_id):
+    try:
+        content_entry = Content.objects.get(pk=content_id)
+    except:
+        raise Http404('Content entry does not exist')
+    return render(request, 'content/detail.html', {'content_entry': content_entry})
